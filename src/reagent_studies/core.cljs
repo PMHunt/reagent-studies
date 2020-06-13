21e2688c2904ffdf40d05f1cf08773d59a3a0057
Phil Hunt <phil@deviantlogic.co.uk>
2020-06-13 11:58:31 +0100

    rendered simple react component

--- src/reagent_studies/core.cljs
+++ src/reagent_studies/core.cljs
@@ -1,9 +1,34 @@
-(ns reagent-studies.core)
+(ns reagent-studies.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
 (enable-console-print!)
 
 (defn hello [] "Hello Baby")
 
-(set! (.-innerHTML (js/document.getElementById "app")) (hello))
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(defn simple-parent []
+  [:div
+   [:p "I include simple-component."]
+   [simple-component]])
+
+(defn hello-component [name]
+  [:p "Hello, " name "!"])
+
+(defn say-hello []
+  [hello-component "world"])
+
+(defn render-simple []
+  (rdom/render
+   [simple-parent]
+   (.-body js/document)))
+
+(set! (.-innerHTML (js/document.getElementById "app")) (render-simple))
 
 (println (hello))
